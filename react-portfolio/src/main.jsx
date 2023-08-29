c53cce1e5e9adac8eff8b0ff9f47600368b2237c
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-08-29 17:29:59 -0400

    added home page

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createHashBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+
+const router = createHashBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
